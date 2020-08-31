95ede6fb72f2fa7078dc5c6567316a3181878817
fantow <1727611626@qq.com>
2020-08-31 20:54:31 +0800

    init

---
+++ IOCComponent/src/main/java/com/fantow/ioc/context/exception/BeanCreateException.java
@@ -0,0 +1,14 @@
+package com.fantow.ioc.context.exception;
+
+/**
+ * @Description:
+ * @Author chenyang270
+ * @CreateDate
+ * @Version: 1.0
+ */
+public class BeanCreateException extends RuntimeException{
+
+    public BeanCreateException(String ex){
+        super(ex);
+    }
+}
---
+++ IOCComponent/src/main/java/com/fantow/ioc/context/exception/PropertyException.java
@@ -0,0 +1,13 @@
+package com.fantow.ioc.context.exception;
+
+/**
+ * @Description:
+ * @Author chenyang270
+ * @CreateDate
+ * @Version: 1.0
+ */
+public class PropertyException extends RuntimeException{
+    public PropertyException(String ex){
+        super(ex);
+    }
+}
